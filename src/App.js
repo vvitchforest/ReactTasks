e830bf3f28eb0ffae5030603993c21251cc29795
irina <irina.konovalova455@gmail.com>
2021-03-18 14:48:38 +0200

    Ajax Task A

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 
 import MediaTable from './components/MediaTable';
 
-const picArray = [
-  {
-    title: 'Title 1',
-    // eslint-disable-next-line max-len
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-
-    title: 'Title 2',
-    // eslint-disable-next-line max-len
-    description: 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/162',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    title: 'Title 3',
-    // eslint-disable-next-line max-len
-    description: 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/163',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
-
 const App = () => {
   return (
-    <MediaTable picArray={picArray}/>
+    <MediaTable />
   );
 };
 
--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,13 +1,27 @@
+import {useState, useEffect} from 'react';
 import MediaRow from './MediaRow';
-import PropTypes from 'prop-types';
 
-const MediaTable = ({picArray}) => {
+
+const MediaTable = () => {
+  const [picArray, setPicArray] = useState([]);
+
+  useEffect(() => {
+    const loadMedia = async () => {
+      const response = await fetch('test.json');
+      const files = await response.json();
+      console.log(files);
+      setPicArray(files);
+    };
+
+    loadMedia();
+  }, []);
+
   return (
     <table>
       <tbody>
         {
           picArray.map((item, index) => {
-            return <MediaRow key={index} file={item} />;
+            return <MediaRow key={index} file={item}/>;
           })
         }
       </tbody>
@@ -15,9 +29,6 @@ const MediaTable = ({picArray}) => {
   );
 };
 
-MediaTable.propTypes = {
-  picArray: PropTypes.array.isRequired,
-};
 
 export default MediaTable;
 
