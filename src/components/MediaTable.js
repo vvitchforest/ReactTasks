7ce133ece51ae36a1c7ab232b3746af797a40332
irina <irina.konovalova455@gmail.com>
2021-03-23 13:26:00 +0200

    api hook

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,28 +1,9 @@
-import {useState, useEffect} from 'react';
-import MediaRow from './MediaRow';
-import {baseUrl} from '../utils/variables';
 
+import MediaRow from './MediaRow';
+import {useAllMedia} from '../hooks/ApiHooks';
 
 const MediaTable = () => {
-  const [picArray, setPicArray] = useState([]);
-
-  useEffect(() => {
-    const loadMedia = async () => {
-      const response = await fetch(baseUrl + 'media');
-      const files = await response.json();
-      console.log(files);
-
-      // 2nd fetch:
-      const media = await Promise.all(files.map(async (item) => {
-        const resp = await fetch(baseUrl + 'media/' + item.file_id);
-        return resp.json();
-      }));
-      setPicArray(media);
-    };
-
-    loadMedia();
-  }, []);
-
+  const picArray = useAllMedia();
   return (
     <table>
       <tbody>
---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const useAllMedia = () => {
+  const [picArray, setPicArray] = useState([]);
+
+  useEffect(() => {
+    const loadMedia = async () => {
+      const response = await fetch(baseUrl + 'media');
+      const files = await response.json();
+      console.log(files);
+
+      // 2nd fetch:
+      const media = await Promise.all(files.map(async (item) => {
+        const resp = await fetch(baseUrl + 'media/' + item.file_id);
+        return resp.json();
+      }));
+      setPicArray(media);
+    };
+
+    loadMedia();
+  }, []);
+
+  return picArray;
+};
+
+export {useAllMedia};
